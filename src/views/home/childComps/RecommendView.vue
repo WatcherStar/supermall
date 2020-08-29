d2afcf5b2177e9daa528b12c687b599175301994
WatcherStar <1815375920@qq.com>
2020-08-29 16:45:25 +0800

    1.1.0

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="recommend">
+		<div v-for="item in recommends" class="recommend-item">
+			<a :href="item.link">
+				<img :src="item.image" alt="">
+				<div>{{item.title}}</div>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"RecommendView",
+		props:{
+			recommends:{
+				type:Array,
+				default(){
+					return []
+				}
+			}
+		},
+		
+	}
+</script>
+
+<style>
+	.recommend {
+		display: flex;
+		font-size: 12px;
+		padding: 10px 0 20px 0;
+		border-bottom: 10px solid #eee;
+	}
+	
+	.recommend-item {
+		flex: 1;
+		text-align: center;
+	}
+	.recommend-item img {
+		width: 70px;
+		height: 70px;
+		margin: 0 auto;
+		margin-bottom: 5px;
+	}
+</style>
